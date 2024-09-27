cc2c931819f9dc3e93b9a031cd66cbca355cb9a7
kavsarr <kevserhuseynova@gmail.com>
2024-09-27 10:08:07 -0400

    change the app to use prodata evaluation pipeline

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from functools import partial
 import gradio as gr
 # Choose ligtheval or harness backend
 #from main_backend_lighteval import run_auto_eval
-from main_backend_harness import run_auto_eval
+from main_backend_prodata import run_auto_eval
 
 from src.display.log_visualizer import log_file_to_html_string
 from src.display.css_html_js import dark_mode_gradio_js
